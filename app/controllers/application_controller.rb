be7015dedbf985f33f1b5a5e886685a91089c983
Scott Silverman <scottms724@gmail.com>
2019-06-22 18:30:29 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,9 +9,6 @@ class App < Sinatra::Base
     end 
     
     post '/teams' do
-      @team_name = params[:team][:name]
-      @team_motto = params[:team][:motto]
-      
       @team = Team.new(params[:team])
       params[:team][:superheroes].each do |details|
         Superhero.new(details)
