9c78f495182da04b68178d839dc24c1e850029c1
Scott Silverman <scottms724@gmail.com>
2019-06-22 19:31:55 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class App < Sinatra::Base
       @superheroes = members.collect do |member_params|
         
         SuperHero.new({name: member_params[:name], power: member_params[:power], bio: member_params[:bio]})
-        
+      end 
       erb :team 
     end 
 
